cb3bf4120445332035f1a310fb6eb4c158936ea7
Mohd Saqueib Ansari <saquibweb@gmail.com>
2022-01-31 14:14:52 +0530

    tweak migration and php support

--- src/migrations/2014_10_00_000001_add_group_column_on_settings_table.php
+++ src/migrations/2014_10_00_000001_add_group_column_on_settings_table.php
@@ -15,7 +15,7 @@ class AddGroupColumnOnSettingsTable extends Migration
     {
         Schema::table('settings', function (Blueprint $table) {
             $table->dropUnique('settings_name_unique');
-            $table->string('group')->default('default');
+            $table->string('group')->after('val')->default('default');
         });
     }
 
