1a7073eae87e2e6f466f54408323042d2784e8b9
Mohd Saqueib Ansari <saquibweb@gmail.com>
2026-08-17 12:29:11 +0530

    Add Laravel 13 support (#45)
    Support Laravel 13 and Testbench 11, modernize PHPUnit test discovery, and test maintained Laravel versions.

--- tests/StorageTest.php
+++ tests/StorageTest.php
@@ -26,9 +26,8 @@ class StorageTest extends TestCase
     /**
      * it sets a new key value in store
      *
-     * @test
      */
-    public function it_sets_a_new_key_value_in_store()
+    public function test_it_sets_a_new_key_value_in_store()
     {
         $this->assertDatabaseMissing('settings', ['app_name' => 'QCode']);
 
@@ -40,9 +39,8 @@ class StorageTest extends TestCase
     /**
      * it dont set if same key value pair exists in store
      *
-     * @test
      */
-    public function it_dont_set_if_same_key_value_pair_exists_in_store()
+    public function test_it_dont_set_if_same_key_value_pair_exists_in_store()
     {
         $this->assertDatabaseMissing('settings', ['app_name' => 'QCode']);
 
@@ -59,9 +57,8 @@ class StorageTest extends TestCase
     /**
      * it updates exisiting setting if already exists
      *
-     * @test
      */
-    public function it_updates_exisiting_setting_if_already_exists()
+    public function test_it_updates_exisiting_setting_if_already_exists()
     {
         $this->settingStorage->set('app_name', 'QCode');
         $this->assertDatabaseHas('settings', ['name' => 'app_name', 'val' => 'QCode']);
@@ -75,9 +72,8 @@ class StorageTest extends TestCase
     /**
      * it removes a setting from storage
      *
-     * @test
      */
-    public function it_removes_a_setting_from_storage()
+    public function test_it_removes_a_setting_from_storage()
     {
         $this->settingStorage->set('app_name', 'QCode');
         $this->assertDatabaseHas('settings', ['name' => 'app_name', 'val' => 'QCode']);
@@ -92,9 +88,8 @@ class StorageTest extends TestCase
     /**
      * it gives default value if nothing setting not found
      *
-     * @test
      */
-    public function it_gives_default_value_if_nothing_setting_not_found()
+    public function test_it_gives_default_value_if_nothing_setting_not_found()
     {
         $this->assertDatabaseMissing('settings', ['app_name' => 'QCode']);
 
@@ -107,9 +102,8 @@ class StorageTest extends TestCase
     /**
      * it gives you saved setting value
      *
-     * @test
      */
-    public function it_gives_you_saved_setting_value()
+    public function test_it_gives_you_saved_setting_value()
     {
         $this->settingStorage->set('app_name', 'QCode');
 
@@ -130,9 +124,8 @@ class StorageTest extends TestCase
     /**
      * it can add multiple settings in if multi array is passed
      *
-     * @test
      */
-    public function it_can_add_multiple_settings_in_if_multi_array_is_passed()
+    public function test_it_can_add_multiple_settings_in_if_multi_array_is_passed()
     {
         $this->settingStorage->set([
             'app_name' => 'QCode',
@@ -149,9 +142,8 @@ class StorageTest extends TestCase
     /**
      * it can use helper function to set and get settings
      *
-     * @test
      */
-    public function it_can_use_helper_function_to_set_and_get_settings()
+    public function test_it_can_use_helper_function_to_set_and_get_settings()
     {
         settings()->set('app_name', 'Cool App');
 
@@ -163,9 +155,8 @@ class StorageTest extends TestCase
     /**
      * it can access setting via facade
      *
-     * @test
      */
-    public function it_can_access_setting_via_facade()
+    public function test_it_can_access_setting_via_facade()
     {
         \Settings::set('app_name', 'Cool App');
 
@@ -177,9 +168,8 @@ class StorageTest extends TestCase
     /**
      * it has a default group name for settings
      *
-     * @test
      */
-    public function it_has_a_default_group_name_for_settings()
+    public function test_it_has_a_default_group_name_for_settings()
     {
         settings()->set('app_name', 'Cool App');
 
@@ -193,9 +183,8 @@ class StorageTest extends TestCase
     /**
      * it can store setting with a group name
      *
-     * @test
      */
-    public function it_can_store_setting_with_a_group_name()
+    public function test_it_can_store_setting_with_a_group_name()
     {
         settings()->group('set1')->set('app_name', 'Cool App');
 
@@ -209,9 +198,8 @@ class StorageTest extends TestCase
     /**
      * it can get setting from a group
      *
-     * @test
      */
-    public function it_can_get_setting_from_a_group()
+    public function test_it_can_get_setting_from_a_group()
     {
         settings()->group('set1')->set('app_name', 'Cool App');
 
@@ -223,9 +211,8 @@ class StorageTest extends TestCase
     /**
      * it give you all settings from default group if you dont specify one
      *
-     * @test
      */
-    public function it_give_you_all_settings_from_default_group_if_you_dont_specify_one()
+    public function test_it_give_you_all_settings_from_default_group_if_you_dont_specify_one()
     {
         settings()->set('app_name', 'Cool App 1');
         settings()->set('app_name', 'Cool App 2');
@@ -237,9 +224,8 @@ class StorageTest extends TestCase
     /**
      * it allows same key to be used in different groups
      *
-     * @test
      */
-    public function it_allows_same_key_to_be_used_in_different_groups()
+    public function test_it_allows_same_key_to_be_used_in_different_groups()
     {
         settings()->group('team1')->set('app_name', 'Cool App 1');
         settings()->group('team2')->set('app_name', 'Cool App 2');
@@ -252,9 +238,8 @@ class StorageTest extends TestCase
     /**
      * it get group settings using facade
      *
-     * @test
      */
-    public function it_get_group_settings_using_facade()
+    public function test_it_get_group_settings_using_facade()
     {
         \Settings::group('team1')->set('app_name', 'Cool App');
 
